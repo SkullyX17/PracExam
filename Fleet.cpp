2a2391a9df29bbb5ddf2b61582ed7b36c4a300cb
Your Name <you@example.com>
2024-05-02 13:55:16 +0930

    done

--- Fleet.cpp
+++ Fleet.cpp
@@ -7,18 +7,20 @@
 Fleet::Fleet()
 {
 
-Car* c = new Car;
-Tesla* t1= new Tesla;
-Tesla* t2= new Tesla;
-Ford* f1= new Ford;
-Ford* f2= new Ford;
-
+Car* c = new Car(30000);
+Tesla* t1= new Tesla('S', 10000);
+Tesla* t2= new Tesla('T', 50000);
+Ford* f1= new Ford(1, 20000);
+Ford* f2= new Ford(2, 40000);
 
     fleet[0]= t1;
-
+    fleet[1]= f1;
+    fleet[2]= c;
+    fleet[3]= f2;
+    fleet[4]= t2;
 
 }
 Car** Fleet::get_fleet()
 {
-
+return fleet;
 }
---
+++ main-3-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Fleet.h"
+#include "Car.h"
+#include "Tesla.h"
+#include "Ford.h"
+using namespace std;
+int main()
+{
+
+Fleet f1;
+cout<<f1.get_fleet()<<endl;
+
+}
