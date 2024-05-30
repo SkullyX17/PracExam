cc53ecf33319be2eb281abf621159eb415f64f69
Your Name <you@example.com>
2024-05-30 13:00:33 +0930

    commit

--- Persona.h
+++ Persona.h
@@ -10,11 +10,21 @@ class Persona:public Spot
 private:
     /* data */
 public:
-    Persona(/* args */);
+    Persona(int x, int y);
+    void shift(int dx, int dy);
+
  
 };
 
-
+Persona::Persona(int x, int y)
+{
+location = make_tuple(x,y);
+_category ='P';
+}
+    void Persona::shift(int dx, int dy)
+    {
+        location= make_tuple(dx,dy);
+    }
 
 
 
